e90e0dfd677ce1f5a2ba274a3d7ce1615a5ee672
Tai-Rocha <taina013@gmail.com>
2022-07-13 15:06:43 -0300

    add first filter 1_filter.R file

---
+++ R/peld/1_filter.R
@@ -0,0 +1,13 @@
+
+
+library(textreadr)
+library(dplyr)
+
+peld_1 <- read_dir("data-raw/peld_raw/Documents/")
+
+peld_1 <- tibble::as_tibble(test_1)
+
+peld_datmerged_1 <- peld_1 |> 
+  dplyr::group_by(document) |> 
+  dplyr::summarise(content = paste(content, collapse = ","))
+  print(head(peld_datmerged_1))
