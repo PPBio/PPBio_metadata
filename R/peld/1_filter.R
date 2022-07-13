88d0842169aa2cb5bfa00697f796350443285d8e
Tai-Rocha <taina013@gmail.com>
2022-07-13 15:40:06 -0300

    updated filter 1

--- R/peld/1_filter.R
+++ R/peld/1_filter.R
@@ -3,6 +3,9 @@
 library(textreadr)
 library(dplyr)
 
+library(tidyverse)
+
+## 1
 peld_1 <- read_dir("data-raw/peld_raw/Documents/")
 
 peld_1 <- tibble::as_tibble(test_1)
@@ -11,3 +14,9 @@ peld_datmerged_1 <- peld_1 |>
   dplyr::group_by(document) |> 
   dplyr::summarise(content = paste(content, collapse = ","))
   print(head(peld_datmerged_1))
+  
+## 2 
+
+dias <- peld_datmerged_1 |> 
+  filter(str_detect(document, '^dias'))
+    
